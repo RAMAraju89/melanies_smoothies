a452a40edc3e10dcb1ea3942304bbbccbdf32707
RAMAraju89 <85574424+RAMAraju89@users.noreply.github.com>
2024-09-04 12:20:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,43 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
+
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
-    """
-)
-
-name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be:', name_on_order)
-
+    """)
 
 cnx = st.connection("snowflake")
-session = cnx.session()
+session =cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width= True)
+
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on Smoothie will be :', name_on_order)
+
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
 #st.stop()
 
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
-
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    my_dataframe,
-    max_selections=5
+    'Choose upto 5 ingredients :'
+    ,my_dataframe
+    ,max_selections =5
 )
-
-# Initialize ingredients_string
 
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
+        ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
